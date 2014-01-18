c50538ec8f5bf78579ebbf7054218c53a54d0698
meanprogrammer <valiantdudz@yahoo.com>
2014-01-18 21:28:10 +0800

    Initial commit

---
+++ Products.AngularJS.Library/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Products.AngularJS.Library.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
